aaf3f7259b0af465e8f6eb684aaba31446ac92ba
Ankur Singh <ankursinghagra@gmail.com>
2024-07-31 19:42:17 +0530

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,48 @@
-var gulp = require('gulp');
+const { series, gulp, src, dest, watch } = require('gulp');
+const clean = require('gulp-clean');
+const browserSync = require('browser-sync').create();
+const browserify = require('gulp-browserify');
+const typescript = require('gulp-tsc');
 
-function defaultTask(cb) {
-  // place code for your default task here
-
-  //copy public files to dist
-  gulp.src('public/*.*').pipe(gulp.dest('dist/'));
-  //copy src files to dist
-  gulp.src('src/*.js').pipe(gulp.dest('dist/'));
-
+async function cleanfiles(cb) {
+  await src('dist/**',{read: false}).pipe(clean());
   cb();
 }
 
-exports.default = defaultTask
+async function htmlcopy(cb) {
+  await src('public/*.*').pipe(dest('dist/'));
+  cb();
+}
+
+
+// process JS files and return the stream.
+async function jscopy(cb) {
+  await src('src/*.ts')
+    .pipe(typescript())
+    .pipe(browserify({insertGlobals : true}))
+    .pipe(dest('dist/'));
+  cb();
+}
+
+// reloading browsers
+async function reload(cb) {
+  browserSync.reload();
+  cb();
+}
+
+function live(){
+  // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        },
+        open: false
+    });
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    watch("src/*.js", series(cleanfiles,htmlcopy,jscopy,reload));
+}
+
+// use default task to launch Browsersync and watch JS files
+exports.default = series(cleanfiles,htmlcopy,jscopy,live);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import { Application } from 'pixi.js';
+
+/*import { Application } from 'pixi.js';
 
 const app = new Application();
 
 await app.init({ background: '#1099bb', resizeTo: window });
-document.body.appendChild(app.canvas);
+document.body.appendChild(app.canvas);*/
+import * as PIXI from 'pixi.js';
+
+const app = new PIXI.Application({height: 600, width: 700});
+globalThis.__PIXI_APP__ = app;
+// add to dom
+document.body.appendChild(app.view);
